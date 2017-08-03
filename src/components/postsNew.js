5c8ced08bb52ce0cd53429ef8e02126816e69629
lukas klingberg <lukas@klingberg.se>
2017-08-03 18:10:58 +0200

    lesson 134 done

---
+++ src/components/postsNew.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
+    renderTagsField(field) {
+        return (
+            <div className="form-group">
+                <label>Tags</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <form>
+                <Field 
+                    label="Title for post"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Content for post"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
+        )
+    }
+}
+
+export default reduxForm({
+    form: 'PostsNewForm',
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducerPosts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  from: formReducer,
 });
 
 export default rootReducer;
